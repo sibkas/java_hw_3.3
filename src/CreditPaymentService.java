76a87e2c98ffc1694b079d65a2218e0daf8dfff7
akovr <akovr@yahoo.com>
2025-07-10 14:17:53 +0700

    исправил формулу

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class CreditPaymentService {
+    public int calculate(int credit, double bid, int years) {
+
+        double monthlyRate = bid / 100 / 12; // месячная ставка
+        int mounths = years * 12; // месяцев в году
+        double numerator = monthlyRate * Math.pow((1 + monthlyRate), mounths); // числитель формулы "Коэффициент аннуитета"
+        double denominator = Math.pow((1 + monthlyRate), mounths) - 1; // знаменатель формулы "Коэффициент аннуитета"
+        double coefficient = numerator / denominator; // коэффициент аннуитета
+        double monthPay = credit * coefficient;
+        return (int) Math.round(monthPay); // округляем до целого
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        int credit = 1_000_000; // сумма кредита
+        double bid = 9.99; // процентная ставка
+
+
+        CreditPaymentService service = new CreditPaymentService();
+
+        int payment1 = service.calculate(credit, bid, 1);
+        System.out.println("Срок 1 год: ежемесячный платёж = " + payment1 + " рублей");
+
+        int payment2 = service.calculate(credit, bid, 2);
+        System.out.println("Срок 2 года: ежемесячный платёж = " + payment2 + " рублей");
+
+        int payment3 = service.calculate(credit, bid, 3);
+        System.out.println("Срок 3 года: ежемесячный платёж = " + payment3 + " рублей");
     }
 }
